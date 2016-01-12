c358f0336fbb35a04b02bee9f8e8bfd1f6fafc67
Víctor Hernández <alu0100697032@ull.edu.es>
2016-01-12 16:15:12 +0000

    PATableConfigReader implemented, added PATableConfig.txt

---
+++ Assets/PATableConfigReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PATableConfigReader : MonoBehaviour {
+
+    public TextAsset asset;
+    private string[][] PATable;
+
+    void Start()
+    {
+        string[] lines = asset.text.Split('\n');
+        PATable = new string[lines.Length][];
+        for (int i = 0; i < lines.Length; i++) {
+            string[] line = lines[i].Split(' ');
+            PATable[i][line.Length] = line;
+        }
+    }
+
+    public string[][] getPATable()
+    {
+        return PATable;
+    }
+
+}
