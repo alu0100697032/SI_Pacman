fde840c938f34f570bd45dfd1c8ad393d75e4bf2
Germán Méndez <gcpmendez@gmail.com>
2016-01-20 12:14:29 +0000

    Implementing AStarMove

---
+++ Assets/AStarMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AStarMove : MonoBehaviour {
+
+    public GameObject maze;
+    public float speed;
+    int[] vecinos = new int[4];
+    private Vector2 dest = Vector2.zero;
+    private Vector2 position = Vector2.zero;
+
+    void Start()
+    {
+        dest = (Vector2)transform.localPosition;
+        position = dest;
+        vecinos = maze.GetComponent<nivel>().getVecinos((int)position.x, (int)position.y);
+    }
+
+    void FixedUpdate()
+    {
+
+
+        //Mueve el pacman teniendo en cuenta la velocidad
+        float step = speed * Time.deltaTime;
+        Vector2 dest2 = Vector2.MoveTowards(transform.localPosition, dest, step);
+        transform.localPosition = dest2;
+        //Si ya ha llegado al destino actualiza los vecinos (solo en las posiciones enteras)
+        if ((dest2.x == position.x + 1) || (dest2.x == position.x - 1) || (dest2.y == position.y + 1) || (dest2.y == position.y - 1))
+        {
+            position = dest;
+            if (maze.GetComponent<nivel>().hayPastilla((int)position.x, (int)position.y))
+            {
+                maze.GetComponent<nivel>().eliminarPastilla((int)position.x, (int)position.y);
+                GetComponent<pacmanLogic>().scoreUp(10);
+            }
+            vecinos = maze.GetComponent<nivel>().getVecinos((int)position.x, (int)position.y);
+        }
+
+        // Anima al pacman
+        Vector2 dir = dest - (Vector2)transform.localPosition;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+}
